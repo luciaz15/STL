086d261f40d3a4e4132d29b3f120d0f900d44618
Lucia <58532876+luciaz15@users.noreply.github.com>
2023-06-21 23:46:41 -0500

    Add files via upload

--- STL_1.sql
+++ STL_1.sql
@@ -1,48 +1,38 @@
--- Clean data, join both dataset to show the total number of samples
-With Counties As 
-(SELECT
-Customer,
-regexp_replace (TRIM(TRIM(Customer, " "),  "."), r'Dist|Dis|District', 'District'), 
-regexp_replace(TRIM(TRIM(Customer, " "),  "."), r'Ext.|Ext|Extension', 'Extension'),
-Safe_cast (LabNo as Integer) as LabNo,
-Month,
-Year, 
-OrderNo
-FROM `STL_orders.STL_2018-2022_3-11` 
-UNION DISTINCT
-SELECT
-Customer, 
-regexp_replace (TRIM(TRIM(Customer, " "),  "."), r'Dist|Dis|District', 'District'), 
-regexp_replace (TRIM(TRIM(Customer, " "),  "."), r'Ext.|Ext|Extension', 'Extension'),
-Safe_cast (LabNo as Integer) as LabNo,
-Month,
-Year, 
-OrderNo
-FROM `STL_orders.STL_2021-2022_v-3-11`)
--- From the new table motnh and year total samples
-SELECT 
-Customer,
-Year,
-Month,
-COUNT (*) AS Total_samples,
-FROM Counties
-#WHERE REGEXP_CONTAINS(Customer, r"Extension District|County Extension|Trail Extension")  
-WHERE LabNo < 100000
-GROUP BY 
-Customer, 
-Year, 
-Month
-ORDER BY Total_samples DESC
+--- SQL exporation data for soil testing lab
 
-
-
-With Counties As 
+--- Joining both tables into one
+With Clients As 
 (SELECT *,
-FROM `geometric-petal-362919.STL_orders.STL_2018-2022_4-1` 
+FROM `STL_orders.STL-2018-2022_4-1` 
 UNION DISTINCT
 SELECT *
-FROM `geometric-petal-362919.STL_orders.STL_2021-2022_4-1`)
+FROM `STL_orders.STL-2021-2022_4-1`)
 -- From the new table
+SELECT 
+*
+FROM Clients
+
+--- Exploring the amount of samples received grouped by years
+
+SELECT Safe_cast (Year as Integer) as Year, COUNT (*) AS number_of_order
+FROM 
+(SELECT Year,  LabNo
+FROM `STL_orders.STL-2018-2022_4-1` 
+UNION DISTINCT
+SELECT Year, LabNo
+FROM `STL_orders.STL-2021-2022_4-1`)
+WHERE  Year > 2017 AND 2023 > Year
+GROUP BY Year
+ORDER BY Year DESC
+
+--- Most frequent client either person or business
+
+With Clients As 
+(SELECT *,
+FROM `STL_orders.STL-2018-2022_4-1` 
+UNION DISTINCT
+SELECT *
+FROM `STL_orders.STL-2021-2022_4-1`)
 SELECT 
 Customer,
 Count (*) AS Total_samples
@@ -50,5 +40,33 @@ FROM Counties
 where sector = "Private"
 Group BY Customer
 Order by Total_samples DESC
+
+--- Time of the year where the most amount of samples are recevied
+
+SELECT Month, COUNT (*) AS number_of_order
+FROM 
+(SELECT Year,  Month, LabNo
+FROM `STL_orders.STL-2018-2022_4-1` 
+WHERE  Year > 2017 AND 2023 > Year
+UNION DISTINCT
+SELECT Year, Month, LabNo
+FROM `STL_orders.STL-2021-2022_4-1`
+WHERE  Year > 2017 AND 2023 > Year)
+GROUP BY Month
+ORDER BY number_of_order DESC
+
+--- Which sector send the most number el samples
+SELECT Sector, COUNT (*) AS number_of_order
+FROM 
+(SELECT Year,  Sector, LabNo
+FROM `STL_orders.STL-2018-2022_4-1` 
+WHERE  Year > 2017 AND 2023 > Year
+UNION DISTINCT
+SELECT Year, Sector, LabNo
+FROM `STL_orders.STL-2021-2022_4-1`
+WHERE  Year > 2017 AND 2023 > Year)
+GROUP BY Sector
+ORDER BY number_of_order DESC
+
 
 
